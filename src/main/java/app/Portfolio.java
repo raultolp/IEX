04f17c7e24aa5c7692edda0b922d37cabd00a4f0
RSiimon <renata.siimon@gmail.com>
2018-04-06 19:45:52 +0300

    lõpuks ometi

--- src/main/java/app/Portfolio.java
+++ src/main/java/app/Portfolio.java
@@ -1,15 +1,24 @@
 package app;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Portfolio {
-    private List<Stock> stocks;
 
-    public Portfolio(List<Stock> stocks) {
-        this.stocks = stocks;
+    //private List<Stock> portfolio= new ArrayList<>();
+    private Map<String, Stock>portfolio=new HashMap<>();
+
+
+
+    public void addStock(Stock stock){
+        portfolio.put(stock.getTicker(), stock);
     }
 
-    public void addStock(Stock stock) {
-        stocks.add(stock);
+    public Stock getStock(String ticker){
+        return portfolio.get(ticker);
     }
+
 }
+
--- src/main/java/app/Stock.java
+++ src/main/java/app/Stock.java
@@ -1,11 +1,41 @@
 package app;
 
 public class Stock {
-    private String name;
-    private double price;
-    private String description;
-    private double valueWhenBought;
-    private double currentValue;
+
+    private final String companyName;
+    private final String ticker;
+    private double curretPrice;
+    private double initialPrice;
+    private double finalPrice;
+
+    public Stock(String companyName, String ticker, double price) {
+        this.companyName = companyName;
+        this.ticker = ticker;
+        this.initialPrice = initialPrice;
+    }
+
+
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public String getTicker() {
+        return ticker;
+    }
+
+
+    public void setCurretPrice(double curretPrice) {
+        this.curretPrice = curretPrice;
+    }
+
+    public void setInitialPrice(double initialPrice) {
+        this.initialPrice = initialPrice;
+    }
+
+    public void setFinalPrice(double finalPrice) {
+        this.finalPrice = finalPrice;
+    }
 
 
 }
