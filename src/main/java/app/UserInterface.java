7ea7b8b378adde30fc60d0f775b024559b41746f
Helena <h@asf.com>
2018-04-08 01:22:36 +0300

    + table

--- src/main/java/app/UserInterface.java
+++ src/main/java/app/UserInterface.java
@@ -12,6 +12,10 @@ import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
@@ -23,6 +27,7 @@ public class UserInterface extends Application {
 
 
     public static void main(String[] args) {
+
         {
             launch(args);
         }
@@ -31,14 +36,15 @@ public class UserInterface extends Application {
     @Override
     public void start(Stage stage) {
 
-        GridPane gridPane = new GridPane();
+        GridPane centerGrid = new GridPane();
+        centerGrid.add(addTableView(), 0, 0);
 
         BorderPane borderPane = new BorderPane();
-        borderPane.setCenter(addFlowPane());
+        borderPane.setCenter(addTableView());
         borderPane.setBottom(addBottomHBox());
 
 
-        Scene scene = new Scene(borderPane, 600, 350);
+        Scene scene = new Scene(borderPane, 600, 550);
 
 
         stage.setTitle("Portfolio");
@@ -64,27 +70,60 @@ public class UserInterface extends Application {
         return hbox;
     }
 
-    private FlowPane addFlowPane() {
+    private TableView<Stock> addTableView() {
 
-        FlowPane flowPane = new FlowPane();
+       /* FlowPane flowPane = new FlowPane();
         flowPane.setPadding(new Insets(5, 0, 5, 0));
         flowPane.setVgap(4);
         flowPane.setHgap(4);
-        flowPane.setPrefWrapLength(230);
-        flowPane.setStyle("-fx-background-color: DAE6F3");
+        flowPane.setPrefWrapLength(230);*/
 
 
-        String[] columnNames = {"Stock symbol", "Price", "Volume"
-                , "Total", "Buy/sell", "Number"};
+        //Stock symbols
+        TableColumn<Stock, String> symbolColumn = new TableColumn<>("Stock");
+        symbolColumn.setMinWidth(100);
+        symbolColumn.setCellValueFactory(new PropertyValueFactory<>("symbol"));
+
+        //Prices
+        TableColumn<Stock, Double> priceColumn = new TableColumn<>("Price");
+        priceColumn.setMinWidth(100);
+        priceColumn.setCellValueFactory(new PropertyValueFactory<>("currentPrice"));
+
+        ///Volume
+        TableColumn<Stock, String> volumeColumn = new TableColumn<>("Volume");
+        volumeColumn.setMinWidth(100);
+
+        ///Total
+        TableColumn<Stock, String> totalColumn = new TableColumn<>("Total");
+        volumeColumn.setMinWidth(100);
+
+        ///Buy/sell
+        ChoiceBox<String> buySellBox = new ChoiceBox<>();
+        buySellBox.getItems().addAll("Buy", "Sell");
+        TableColumn<Stock, String> tradeColumn = new TableColumn<>("Buy/sell");
+        volumeColumn.setMinWidth(100);
+
+        ///Amount
+        TableColumn<Stock, String> numberColumn = new TableColumn<>("Number");
+        volumeColumn.setMinWidth(100);
+
+        TableView table = new TableView<Stock>();
+        table.setEditable(true);
+        table.setItems(getStocksFromPortfolio());
+        table.getColumns().addAll(symbolColumn, priceColumn, volumeColumn, totalColumn, tradeColumn, numberColumn);
 
 
-        flowPane.getChildren().addAll();
+        //flowPane.getChildren().addAll(table);
 
-        return flowPane;
+        return table;
     }
 
-    public ObservableList<Stock> getProduct() {
-        Map<String, Stock> hm = portfolio.getPortfolio();
+    private ObservableList<Stock> getStocksFromPortfolio() {
+        //Map<String, Stock> hm = portfolio.getPortfolio();
+        Stock stock1 = new Stock("AAPL");
+        Map<String, Stock> hm = new HashMap<>();
+        hm.put("AAPL", stock1);
+      //  Portfolio portfolio = new Portfolio(hm);
 
         ObservableList<Stock> stocks = FXCollections.observableArrayList();
 
@@ -92,6 +131,7 @@ public class UserInterface extends Application {
             Stock stock = hm.get(key);
             stocks.add(stock);
         }
+        // System.out.println(stocks);
         return stocks;
     }
 
