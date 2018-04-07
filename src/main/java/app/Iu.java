6b2b3a65bdbac07afe2fb18c80b10a2aefdffdc4
rault <raul.tolp@gmail.com>
2018-04-07 21:28:26 +0300

    lisasin menüü kirjelduse

--- src/main/java/app/Iu.java
+++ src/main/java/app/Iu.java
@@ -12,23 +12,22 @@ import java.net.URL;
 
 public class Iu {
     public static void main(String[] args) {
-        System.out.println("Ugh");
+        final String[] mainMenu = {" 1 - Add user: add <userName>\n",
+                " 2 - List users\n",
+                " 3 - Set active user: set <userName>\n",
+                " 4 - Buy stock: buy <stock>\n",
+                " 5 - Sell stock: sell <stock>\n",
+                " 6 - View user portfolio\n",
+                " 7 - View stock data base data: base <stock>\n",
+                " 8 - View stock historical data graph: historical <stock>\n",
+                " 9 - View all portfolios progress graph\n",
+                "10 - Refresh data from web\n",
+                "11 - Save data\n" };
 
-
-        String symbol="AAPL";
-
-        Stock aapl=new Stock("AAPL");
-
-        System.out.println(aapl.getCompanyName());
-
-        aapl.getLatestPrice();
-
+//        String symbol="AAPL";
+//        Stock aapl=new Stock("AAPL");
+//        System.out.println(aapl.getCompanyName());
+//        aapl.getLatestPrice();
 
     }
-
-
-
-
-
-
 }
--- src/main/java/app/User.java
+++ src/main/java/app/User.java
@@ -1,13 +1,13 @@
 package app;
 
 public class User {
-
+    private String userName;
     private Portfolio portfolio;
     private double cashmoneybiatcheees;
 
-    public User(Portfolio portfolio, double cashmoneybiatcheees) {
+    public User(String userName, Portfolio portfolio, double cashmoneybiatcheees) {
+        this.userName = userName;
         this.portfolio = portfolio;
         this.cashmoneybiatcheees = cashmoneybiatcheees;
     }
-
 }
