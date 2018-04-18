9dda8dd58a2bbe810cd893d37fe166699ba51022
Helena <h@asf.com>
2018-04-18 15:46:13 +0300

    added CH interface and action example

---
+++ src/main/java/app/CommandHandler.java
@@ -0,0 +1,6 @@
+package app;
+
+public interface CommandHandler {
+    void handle(String command) throws Exception;
+}
+
---
+++ src/main/java/app/actions/XX.java
@@ -0,0 +1,13 @@
+package app.actions;
+
+
+import app.CommandHandler;
+
+public class XX implements CommandHandler {
+    @Override
+    public void handle(String command) throws Exception {
+        if (command.startsWith("")) {
+            //do sth
+        }
+    }
+}
