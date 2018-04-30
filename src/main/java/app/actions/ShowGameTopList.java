560b321add5dbfb49cad3f908a3012a072892f17
raultolp <raul.tolp@gmail.com>
2018-04-30 18:44:30 +0300

    minor typo fixes

--- src/main/java/app/actions/ShowGameTopList.java
+++ src/main/java/app/actions/ShowGameTopList.java
@@ -30,13 +30,13 @@ public class ShowGameTopList implements CommandHandler {
         Collections.sort(userList);
 
         System.out.println("\nTOP LIST:");
-        System.out.println(MyUtils.createHeader(String.format("%-5s%-12s%16s%30s",
-                "RANK", "USER", "PORTF.VALUE", "INCREASE IN VALUE")));
+        System.out.println(MyUtils.createHeader(String.format("%-5s%-15s%16s%30s",
+                "RANK", "USER", "PORTFOLIO VALUE", "INCREASE IN VALUE")));
 
         for (int i = 1; i < userList.size() + 1; i++) {
             User user = userList.get(i - 1);
 
-            System.out.println(String.format("%4d. %-12s %10.2f USD %13.2f USD (%8.3f%%)",
+            System.out.println(String.format("%4d. %-12s %13.2f USD %13.2f USD (%8.3f%%)",
                     i, user.getUserName(),
                     user.getPortfolioTotalValue(),
                     user.getValueIncrease(),
