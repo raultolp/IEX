f49871178f6efcf82c08057fe01fd3096e72e8b9
Helena <h@asf.com>
2018-05-08 20:12:27 +0300

    minor fix in while

--- src/main/java/app/client/Client.java
+++ src/main/java/app/client/Client.java
@@ -14,8 +14,8 @@ public class Client {
         System.out.println("connecting to server");
         Socket socket = new Socket("localhost", 1337);
 
-        try (DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-             DataInputStream in = new DataInputStream(socket.getInputStream())) {
+        try ( DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+              DataInputStream in = new DataInputStream(socket.getInputStream()) ) {
 
             //SENDING CLIENT COMMANDS AND RECEIVING ANSWERS FROM SERVER:
             Thread.sleep(1000);
@@ -24,7 +24,7 @@ public class Client {
             thread1.start();
             thread2.start();
 
-            while (thread1.isAlive() && thread2.isAlive()) {
+            while (true) {
                 if (!thread2.isAlive() && thread1.isAlive()) {
                     thread1.interrupt();
                     //thread1.join();
@@ -34,7 +34,7 @@ public class Client {
                     thread2.interrupt();
                     //thread2.join();
                     break;
-                } else if (!thread2.isAlive() && !thread1.isAlive()) {
+                } else if (!thread2.isAlive()) {
                     //thread1.join();
                     //thread2.join();
                     thread1.interrupt();
@@ -44,10 +44,8 @@ public class Client {
             }
 
         } finally {
-            socket.close();
             System.out.println("See you soon!");
-            return;
-
+            socket.close();
         }
 
     }
