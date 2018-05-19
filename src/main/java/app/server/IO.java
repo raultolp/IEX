8e74bbe4dee57200691aae0dfeff9b54f94cd4a2
Helena <h@asf.com>
2018-05-19 11:42:28 +0300

    replaced getters with close

--- src/main/java/app/server/IO.java
+++ src/main/java/app/server/IO.java
@@ -1,9 +1,7 @@
 package app.server;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.util.Scanner;
+
 
 public interface IO {
     void println(String message) throws IOException;
